eb1190c03c8a8323f2635c2ac4d732f30fab2466
DaniFins03 <danifinsbiomed@gmail.com>
2025-07-12 12:06:31 -0300

    Alterações_main_12/07

---
+++ main.py
@@ -0,0 +1,9 @@
+from bio.ler_fasta import ler_fasta 
+
+caminho_do_arquivo ="./arquivos/Flaviviridae-genomes.fasta"
+organismos_do_fasta = ler_fasta(caminho_do_arquivo)
+
+print ("Os organismos desse arquivo são:" )
+
+for organismo in organismos_do_fasta:
+    print(organismo.nome)
