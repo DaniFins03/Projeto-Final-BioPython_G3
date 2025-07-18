d9b62879bf0263609551d1912189570947baf8f0
Larissa-Carvalho98 <larissav646@gmail.com>
2025-07-18 09:08:34 -0300

    feat: adicionando funcoes complementar e complentar_rev

--- bio/sequencia.py
+++ bio/sequencia.py
@@ -20,3 +20,31 @@ class Sequencia:
 
     def __getitem__(self, index):
         return self.sequencia.__getitem__(index)
+    
+    def calcular_tamanho(self):
+        return len(self.sequencia)
+
+    def complementar(self):
+        complementos = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
+        complementar = ""
+        for base in self.sequencia:
+            if base in complementos:
+                complementar += complementos[base]
+            else:
+                complementar += base  
+        return Sequencia(complementar)
+
+    def complementar_reversa(self):
+        complementar_rev = ""
+        for base in self.sequencia[::-1]:
+            if base == "A":
+                complementar_rev += "T"
+            elif base == 'T':
+                complementar_rev += "A"
+            elif base == 'C':
+                complementar_rev += "G"
+            elif base == "G":
+                complementar_rev += "C"
+            else:
+                complementar_rev += base
+        return Sequencia(complementar_rev)
